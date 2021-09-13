3ca5168d91aa464eb37fdc16bb012cd361a497d7
andreyna1808 <andreyna.m.carvalho@gmail.com>
2021-09-12 21:48:14 -0300

    Rodando até 12/14 palavras no limite dos 60 segundos

--- sript.js
+++ sript.js
@@ -1,45 +1,31 @@
-// CRONOMETRO --- Decidi não continuar, pois quando aperta na função o cronometro para;
-let mm = 0;
-let ss = 0;
+// CRONOMETRO --- 
+let digitalElement = document.querySelector('.digital');
+let sElement = document.querySelector('.p_s');
+let mElement = document.querySelector('.p_m');
+let hElement = document.querySelector('.p_h');
 
-let tempo = 1000;
-let cron;
+function updateClock(){
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
 
-//Inicia o cronometro
-function start() {
-    cron = setInterval(() => { timer(); }, tempo);
-}
-//Para o cronometro 
-function pause() {
-    clearInterval(cron);
-}
-//Para o cronometro e limpa as variáveis
-function stop() {
-    clearInterval(cron);
-    mm = 0;
-    ss = 0;
-
-document.getElementById('counter').innerText = '00:00';
+    digitalElement.innerHTML = `${doiszeros(hour)}:${doiszeros(minute)}:${doiszeros(second)}`;
 }
 
-//Faz a contagem do tempo e exibição
-function timer() {
-    ss++; 
-
-    if (ss == 59) { ss = 0; mm++;}
-
-    //Cria uma variável com o valor tratado HH:MM:SS
-    let format = (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss);
-    
-    //Insere o valor tratado no elemento counter
-    document.getElementById('counter').innerText = format;
-
-    //Retorna o valor tratado
-    return format;
+function doiszeros(time){
+    if(time < 10){
+        return '0' + time;
+    }
+    else {
+        return time;
+    }
 }
+setInterval(updateClock, 1000);
+updateClock();
 
 
-// VARIÁVEIS 
+// VARIÁVEIS DO ANAGRAMAS
 const resultsDiv = document.getElementById('results');
 const anagramas5 = document.getElementById("anagramas5");
 const anagramas2 = document.getElementById('anagramas2');
@@ -79,61 +65,38 @@ const getAnagramsSets = () => {
    return anagrama;
 };
 
-const getSetsBySize = (minSize, maxSize) => {
-    const anagramSets = getAnagramsSets()
+const tamanho = (minSize, maxSize) => {
+    const conjutos = getAnagramsSets()
     let filteredSets = {}
 
-    for (let key in anagramSets) {
+    for (let key in conjutos) {
 
         if (key.length <= maxSize && key.length >= minSize) {
-            filteredSets[key] = anagramSets[key]
+            filteredSets[key] = conjutos[key]
         }
     }
     return filteredSets
 }
 
-
-
-// COMANDO PARA O ANAGRAMA DE 3 PALAVRAS 
-// PRECISA MELHORAR O DESEMPENHO PARA RODAR PALAVRAS GRANDES 
-// COMANDO PARA O ANAGRAMA DE 5 OU +
-function getSetsOfFiveAnagrams(){
-    let setsOfFive = [];
-    let anagramSets = getAnagramsSets();
-
-    for (let key in anagramSets) {
-
-        if (anagramSets[key].length >= 5) {
-            setsOfFive.push(anagramSets[key]);
-        };
-    };
-    return JSON.stringify(setsOfFive);
-};
-
-anagramas5.addEventListener("click", function () {
-    showResults(getSetsOfFiveAnagrams());
-  });
-
-
-//LÓGICA PARA 2 PALAVRAS
-function containsExactly(value, reference){
+//LÓGICA
+function conteudo_(valor, reference){
     let contain = true;
     let newArray = reference.split('');
 
-    for (let i = 0; i < value.length; i++) {
-        if (newArray.indexOf(value[i]) === -1){
+    for (let i = 0; i < valor.length; i++) {
+        if (newArray.indexOf(valor[i]) === -1){
             contain = false
         };
-        newArray.splice(newArray.indexOf(value[i]), 1);
+        newArray.splice(newArray.indexOf(valor[i]), 1);
     };
     return contain;
 };
 
-const splicer  = (value, stringToBeSpliced) => {
+const splicer  = (valor, stringToBeSpliced) => {
     let arrayToBeSpliced = stringToBeSpliced.split('');
 
-    for (let index = 0; index < value.length; index++) {
-        let letter = value[index];
+    for (let i = 0; i < valor.length; i++) {
+        let letter = valor[i];
         let letterIndex = arrayToBeSpliced.indexOf(letter);
 
         arrayToBeSpliced.splice(letterIndex, 1);
@@ -143,21 +106,21 @@ const splicer  = (value, stringToBeSpliced) => {
 };
 
 // COMANDO PARA O ANAGRAMA DE 2 PALAVRAS
-function get2WordsAnagrams(value){
-    let respost = letras(value);
-    let anagramSets = getSetsBySize(2, respost.length - 2);
+function anagramTwo(valor){
+    let respost = letras(valor);
+    let conjutos = tamanho(2, respost.length - 2);
     let resultsArr = [];
-    let usedAnagrams = [];
+    let usoAnagram = [];
 
-    for (let key in anagramSets) { 
+    for (let key in conjutos) { 
 
-        if (containsExactly(key, respost)){ 
+        if (conteudo_(key, respost)){ 
             let splicedWord = splicer(key, respost);
 
-            if (anagramSets[splicedWord] !== undefined && !usedAnagrams.includes(key)) {
-                resultsArr.push(`${anagramSets[key]} + ${anagramSets[splicedWord]}`)
-                usedAnagrams.push(key);
-                usedAnagrams.push(splicedWord);
+            if (conjutos[splicedWord] !== undefined && !usoAnagram.includes(key)) {
+                resultsArr.push(`${conjutos[key]} + ${conjutos[splicedWord]}`)
+                usoAnagram.push(key);
+                usoAnagram.push(splicedWord);
             };
         };  
     };
@@ -166,39 +129,61 @@ function get2WordsAnagrams(value){
 
 anagramas2.addEventListener("click", function () {
     let typedText = document.getElementById("input").value;
-    showResults(get2WordsAnagrams(typedText));
+    showResults(anagramTwo(typedText));
 });
+
+// COMANDO PARA 3 PALAVRAS - COM ATÉ 14 LETRAS
+function anagramthree(valor){
+    let palavras_ord = letras(valor);
+    let conjutos = tamanho(2, palavras_ord.length);
+    let Anagramas = [];
+    let usoAnagram = [];
+
+    for (let key in conjutos) {
+
+        if (conteudo_(key, palavras_ord)){ 
+            usoAnagram.push(key)  
+            let usedMiddleAnagram = []
+            let letrasSep = splicer(key, palavras_ord);
+
+            for (let secondKey in conjutos) {
+
+                if (secondKey.length <= letrasSep.length - 3 && conteudo_(secondKey,letrasSep)){
+                    usedMiddleAnagram.push(secondKey)
+                    let secondSplicedWord = splicer(secondKey, letrasSep)
+                    Anagramas.push(`${conjutos[key]} + ${conjutos[secondKey]} + ${conjutos[secondSplicedWord]}`)
+                    };
+                }
+        };  
+    };
+    return JSON.stringify(Anagramas);
+};
+
+anagramas3.addEventListener("click", function () {
+    let typedText = document.getElementById("input").value;
+    showResults(anagramthree(typedText));
+});
+
+// COMANDO PARA O ANAGRAMA DE 5 OU +
+function anagramFive(){
+    let setsOfFive = [];
+    let conjutos = getAnagramsSets();
+
+    for (let key in conjutos) {
+
+        if (conjutos[key].length >= 5) {
+            setsOfFive.push(conjutos[key]);
+        };
+    };
+    return JSON.stringify(setsOfFive);
+};
+
+anagramas5.addEventListener("click", function () {
+    showResults(anagramFive());
+  });
+
 
 // LIMPAR OS TEXTOS
 resetButton.addEventListener('click', function () {
     resultsDiv.innerHTML = ""
 })
-
-function get3WordsAnagrams(value){
-    let sortedWords = letras(value);
-    let anagramSets = getSetsBySize(2, sortedWords.length);
-    let anagramsArr = [];
-    let usedAnagrams = [];
-
-    for (let key in anagramSets) {
-
-        if (containsExactly(key, sortedWords)){ 
-            usedAnagrams.push(key)  
-            let letrasSep = splicer(key, sortedWords);
-
-            for (let secondKey in anagramSets) {
-
-                if (secondKey.length <= letrasSep.length - 2 && containsExactly(secondKey,letrasSep)){
-                    let secondSplicedWord = splicer(secondKey, letrasSep)
-                    anagramsArr.push(`${anagramSets[key]} + ${anagramSets[secondKey]} + ${anagramSets[secondSplicedWord]}`)
-                }
-            } 
-        };  
-    };
-    return JSON.stringify(anagramsArr);
-};
-
-anagramas3.addEventListener("click", function () {
-    let typedText = document.getElementById("input").value;
-    showResults(get3WordsAnagrams(typedText));
-});
